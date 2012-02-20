a1db552a6e6bf2c2f1c7c06379fb7bb193c2578b
Jonathan Colby <jcolby@ebay.de>
2012-02-20 17:20:35 +0100

    update

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,8 +7,7 @@ class BootStrap {
         new NetscalerAppliance(dataCenter: 46, protocol: 'HTTPS', ipAddress: '10.46.28.251', username: 'apiuser', password: 't313F0n', role: 'ACTIVE' ).save()
         new NetscalerAppliance(dataCenter: 47, protocol: 'HTTPS', ipAddress: '10.47.28.251', username: 'apiuser', password: 't313F0n', role: 'ACTIVE' ).save()
         new NetscalerAppliance(dataCenter: 44, protocol: 'HTTPS', ipAddress: '10.44.28.251', username: 'apiuser', password: 't313F0n', role: 'ACTIVE', managed: false ).save()
-
-
+        new NetscalerAppliance(dataCenter: 38, protocol: 'HTTPS', ipAddress: '10.38.28.251', username: 'apiuser', password: 't313F0n', role: 'ACTIVE').save()
 
     }
     def destroy = {
