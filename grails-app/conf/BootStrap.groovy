2010bbc2f0b064c38a2ee95c9b95171a2ac5eb5e
Jonathan Colby <jcolby@team.mobile.de>
2014-05-05 09:54:29 +0200

    various changes for nitro v10

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,7 +5,9 @@ class BootStrap {
     def init = { servletContext ->
 
         //new NetscalerAppliance(dataCenter: 46, protocol: 'HTTPS', ipAddress: '10.46.28.251', username: 'apiuser', password: 't313F0n', role: 'ACTIVE', managed: true).save()
+        new NetscalerAppliance(dataCenter: 46, protocol: 'HTTPS', ipAddress: 'mo-lb001', username: 'apiuser', password: 't313F0n', role: 'ACTIVE', managed: true).save()
         //new NetscalerAppliance(dataCenter: 47, protocol: 'HTTPS', ipAddress: '10.47.28.251', username: 'apiuser', password: 't313F0n', role: 'ACTIVE', managed: true).save()
+        new NetscalerAppliance(dataCenter: 47, protocol: 'HTTPS', ipAddress: 'mo-lb002', username: 'apiuser', password: 't313F0n', role: 'ACTIVE', managed: true).save()
         //new NetscalerAppliance(dataCenter: 44, protocol: 'HTTPS', ipAddress: '10.44.28.251', username: 'apiuser', password: 't313F0n', role: 'ACTIVE', managed: false).save()
         //new NetscalerAppliance(dataCenter: 38, protocol: 'HTTPS', ipAddress: '10.38.28.251', username: 'apiuser', password: 't313F0n', role: 'ACTIVE', managed: true).save()
         new NetscalerAppliance(dataCenter: 38, protocol: 'HTTPS', ipAddress: 'mo-lb003', username: 'apiuser', password: 't313F0n', role: 'ACTIVE', managed: true).save()
